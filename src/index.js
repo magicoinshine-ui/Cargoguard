39e1fb31f169723beedcaf67cd51c75442b923c9
Magicoinshine <magicoinshine@gmail.com>
2026-08-06 00:44:57 -0300

    Add Render web server

--- src/index.js
+++ src/index.js
@@ -1,25 +1,62 @@
 require("dotenv").config();
 
+const express = require("express");
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
 const eventHandler = require("./handlers/eventHandler");
 const commandHandler = require("./handlers/commandHandler");
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildModeration
-    ]
+
+// ===============================
+// SERVIDOR HTTP PARA O RENDER
+// ===============================
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("CargoGuard online!");
 });
 
+
+app.listen(3000, () => {
+    console.log("🌐 Servidor HTTP ativo na porta 3000");
+});
+
+
+// ===============================
+// DISCORD BOT
+// ===============================
+
+const client = new Client({
+
+    intents: [
+
+        GatewayIntentBits.Guilds,
+
+        GatewayIntentBits.GuildMembers,
+
+        GatewayIntentBits.GuildModeration
+
+    ]
+
+});
+
+
 // Coleção de comandos
+
 client.commands = new Collection();
 
+
 // Carrega eventos
+
 eventHandler(client);
 
+
 // Carrega comandos
+
 commandHandler(client);
 
+
 // Login
+
 client.login(process.env.TOKEN);
